237ff8d0728faccbe5951784e42bee37c84ca7fd
Rishav <rishavraj4273@gmail.com>
2024-07-10 23:54:03 +0530

    vercel config

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const indexRouter = require("./routes/index");
 const app = express();
 
 app.set("view engine","ejs");
+app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use(indexRouter);
