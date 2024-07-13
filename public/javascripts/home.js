08e4409aba12a95211b37cbc3f52ba92fec33cbc
Rishav <rishavraj4273@gmail.com>
2024-07-13 16:53:13 +0530

    frontend-fix

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -23,6 +23,10 @@ dropArea.addEventListener("drop",(e)=>{
 });
 
 submitButton.addEventListener ("click",async ()=>{
+    if (!inputFile.files[0]) {
+        alert('Please upload an image before submitting.');
+        return;
+    }
     let output = await submitFile();
     displayOutput(output);
 });
@@ -30,13 +34,33 @@ submitButton.addEventListener ("click",async ()=>{
 
 function displayOutput(data){
     let outputDiv = document.getElementById('output');
-    // let html = '<h2>Here are 5 captions for your Image</h2><ul>';
-    // output.forEach((item, index) => {
-    //     html += `<li><strong>${index + 1}.</strong> ${item}</li>`;
-    // });
-    // html += '</ul>';
-    outputDiv.innerHTML = data;
+    // outputDiv.innerHTML = data.join('<br><br>');
+    outputDiv.innerHTML = '';
+    let head = document.createElement('h5');
+    outputDiv.appendChild(head);
+    head.classList.add('text-center', 'mb-2');
+    head.textContent = 'Here are five caption generate for your image';
+    data.forEach(item => {
+        let outerDiv = document.createElement('div');
+        outerDiv.classList.add('sub-output', 'h-max', 'w-full', 'bg-gray-200', 'rounded-xl');
+
+        let innerDiv = document.createElement('div');
+        innerDiv.classList.add('h-max', 'flex', 'justify-end');
+        
+        let p = document.createElement('p');
+        p.textContent = item;
+        p.classList.add('caption', 'mb-2', 'ml-2');
+        let i = document.createElement('i');
+        i.classList.add('bi', 'bi-copy', 'h-min', 'text-black', 'text-base', 'cursor-pointer', 'bg-gray-300', 'hover:bg-gray-400', 'px-1.5', 'rounded-md');
+        i.id = 'copy';
+
+        innerDiv.appendChild(p);
+        innerDiv.appendChild(i);
+        outerDiv.appendChild(innerDiv);
+        outputDiv.appendChild(outerDiv);
+    });
 }
+
 async function submitFile(){
     let imgFile = inputFile.files[0];
     let mimeType = imgFile.type;
@@ -44,6 +68,7 @@ async function submitFile(){
     if (mimeType !== 'image/png' && mimeType !== 'image/jpeg'){
         return alert("File not supported!!!");
     }
+
     let formData = new FormData();
     formData.append('image', imgFile);
     let response = await fetch('/upload', {
