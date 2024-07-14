a0e5accffea12b04ef13e354ebc599bff7f1ce81
Rishav <rishavraj4273@gmail.com>
2024-07-14 12:02:46 +0530

    permissions

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const path = require("path")
-const indexRouter = require("./routes/index");
 const app = express();
+const fs = require("fs");
+const indexRouter = require("./routes/index");
 
 app.set("view engine","ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname,'public')));
+
+fs.chmod('uploads', 0o777, (err) => {
+    if (err) throw err;
+});
 
 app.use(indexRouter);
 
